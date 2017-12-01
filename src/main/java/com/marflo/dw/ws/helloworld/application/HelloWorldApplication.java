30d6e495d7c789d7c90163789058c28e563c0c9a
Martin Flodin <martin.flodin@klarna.com>
2017-12-01 14:32:42 +0100

    fix local config

--- src/main/java/com/marflo/dw/ws/helloworld/application/HelloWorldApplication.java
+++ src/main/java/com/marflo/dw/ws/helloworld/application/HelloWorldApplication.java
@@ -6,9 +6,11 @@ import com.marflo.dw.ws.helloworld.resources.HelloWorldResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
-
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldApplication.class);
     @Override
     public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
         // nothing to do yet
@@ -17,12 +19,12 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
     @Override
     public void run(HelloWorldConfiguration configuration,
                     Environment environment) {
-
         environment.healthChecks().register("template", new TemplateHealthCheck());
         environment.jersey().register(new HelloWorldResource());
     }
 
     public static void main(String... args) throws Exception {
+        LOG.info("Application starting");
         new HelloWorldApplication().run(args);
     }
 }
--- src/main/java/com/marflo/dw/ws/helloworld/resources/HelloWorldResource.java
+++ src/main/java/com/marflo/dw/ws/helloworld/resources/HelloWorldResource.java
@@ -1,6 +1,9 @@
 package com.marflo.dw.ws.helloworld.resources;
 
 import com.codahale.metrics.annotation.Timed;
+import com.marflo.dw.ws.helloworld.application.HelloWorldApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,10 +15,12 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class HelloWorldResource {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldResource.class);
     @GET
     @Timed
     @Produces(MediaType.APPLICATION_JSON)
     public Response get() {
+        LOG.info("Received request");
         return Response.ok().entity("{\"message\" : \"hello world\"}").build();
     }
 }
